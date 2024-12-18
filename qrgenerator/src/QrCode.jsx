6aa7b2bfbb680b64fb07deb3185bfe380c4f85f4
krishnamoorthy_selvaraj <krishnamoorthy.selvaraj2477@gmail.com>
2024-12-18 22:02:24 +0530

    style button

--- qrgenerator/src/QrCode.jsx
+++ qrgenerator/src/QrCode.jsx
@@ -2,6 +2,7 @@
 export const QrCode = () => {
   return (
     <div className="app-container">
+        <h1>QR CODE GENERATOR</h1>
         <img src="vite.svg" className="qr-code-image"  />
         <div>
             <label htmlFor="dataInput" className="input-label"> Data for QR Code: </label>
@@ -10,10 +11,12 @@ export const QrCode = () => {
             <label htmlFor="sizeInput" className="input-label"> Image Size(e.g., 150): </label>
             <input type="text" id="sizeInput" placeholder="Enter Image Size " />
 
-            <button>Generate QR Code</button>
-            <button>DOwnload QR Code</button>
+            <button className="generate-button">Generate QR Code</button>
+            <button className="download-button">DOwnload QR Code</button>
         </div>
-
+        <p className="footer">
+            Designed by <a href="#">Krishnamoorthy Selvaraj</a>
+        </p>
     </div>
   )
 }
