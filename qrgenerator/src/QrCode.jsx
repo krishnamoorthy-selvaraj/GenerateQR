25408ecba407e6cce9d827eb7f9feb777d6e1f55
krishnamoorthy_selvaraj <krishnamoorthy.selvaraj2477@gmail.com>
2024-12-18 21:40:26 +0530

    style the label and input box

--- qrgenerator/src/QrCode.jsx
+++ qrgenerator/src/QrCode.jsx
@@ -1,6 +1,19 @@
 
 export const QrCode = () => {
   return (
-    <div>QrCode</div>
+    <div className="app-container">
+        <img src="vite.svg" className="qr-code-image"  />
+        <div>
+            <label htmlFor="dataInput" className="input-label"> Data for QR Code: </label>
+            <input type="text" id="dataInput" placeholder="Enter Data for QR Code" />
+
+            <label htmlFor="sizeInput" className="input-label"> Image Size(e.g., 150): </label>
+            <input type="text" id="sizeInput" placeholder="Enter Image Size " />
+
+            <button>Generate QR Code</button>
+            <button>DOwnload QR Code</button>
+        </div>
+
+    </div>
   )
 }
